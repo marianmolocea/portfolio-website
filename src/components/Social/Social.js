29021a2f94a46bec0a151f5efe35e4cc3822288e
Marian Molocea <marian.mcrt@gmail.com>
2020-03-18 15:40:08 +0000

    Removed the lables for the social links

--- src/components/Social/Social.js
+++ src/components/Social/Social.js
@@ -5,10 +5,10 @@ export default function Social() {
   return (
     <div className="Social">
       <a href="https://github.com/marianmolocea" target="blank">
-        <ion-icon name="logo-github">GitHub</ion-icon>
+        <ion-icon name="logo-github"></ion-icon>
       </a>
       <a href="https://www.linkedin.com/in/marian-molocea/" target="blank">
-        <ion-icon name="logo-linkedin">Linked In</ion-icon>
+        <ion-icon name="logo-linkedin"></ion-icon>
       </a>
     </div>
   )

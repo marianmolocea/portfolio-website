f8af6b8fd47f76604bf1e89eebf4db1d91d35ecd
Marian Molocea <marian.mcrt@gmail.com>
2020-03-26 16:43:16 +0000

    Added links instead of buttons

--- src/components/PageView/PageView.js
+++ src/components/PageView/PageView.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React from 'react'
+import {Route} from 'react-router-dom'
 import NavigationBar from '../NavigationBar/NavigationBar'
 import Logo from '../Logo/Logo'
 import Home from '../Home/Home'
@@ -10,45 +11,20 @@ import Contact from '../Contact/Contact'
 
 import './PageView.css';
 
-export default class PageView extends Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
-
-    changePageContent(e) {
-      let value = e.target.getAttribute('value');
-      let element = document.querySelector(`.${value}`)
-      let classNames = element.getAttribute('class');
-      let child = document.querySelector('.NavigationBar').childNodes
-      child.forEach(el => {
-        if(el.getAttribute('class').includes('active') && (el !== classNames)){
-          el.classList.remove('active')
-        }
-      })
-      if(!classNames.includes('active')){
-        element.classList.add('active');   
-      } 
-      this.setState({ value });
-    };
-
-  render() {
-    return (
-      <div className="PageView">
-        <Logo />
-        <Social />
-        <div className="container">
-          {Object.values(this.state).length === 0 && <Home />}
-          {this.state.value === 'home' && <Home />}
-          {this.state.value === 'about' && <About />}
-          {this.state.value === 'skills' && <Skills />}
-          {this.state.value === 'portfolio' && <Projects />}
-          {this.state.value === 'contact' && <Contact />}
-        </div>
-        <NavigationBar 
-          pageContentTrigger={(e) => this.changePageContent(e)}
-        />
+export default function PageView() {
+  return (
+    <div className="PageView">
+      <Logo />
+      <Social />
+      <div className="container">
+        <Route exact path="/" component={Home}></Route>
+        <Route exact path="/home" component={Home}></Route>
+        <Route exact path="/about" component={About}></Route>
+        <Route exact path="/skills" component={Skills}></Route>
+        <Route exact path="/portfolio" component={Projects}></Route>
+        <Route exact path="/contact" component={Contact}></Route>
       </div>
-    )
-  }
+      <NavigationBar />
+    </div>
+  )
 }

68a020130286072974372432d44b22c6db19355e
Marian Molocea <marian.mcrt@gmail.com>
2020-03-26 21:52:42 +0000

    increase the z-index for the logo and social icons

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import logo from '../../images/logo600x600.png'
 import './Logo.css'
+import { Link } from 'react-router-dom'
 
 export default function Logo() {
   return (
-    <img className="Logo" src={logo} alt="Marian Molocea" />
+    <Link to="/home">
+      <img className="Logo" src={logo} alt="Marian Molocea" />
+    </Link>
   )
 }

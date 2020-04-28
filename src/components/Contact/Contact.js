7576ecbd9c793a63bc7d839fde71d8f4df952b02
marianmolocea <marian.mcrt@gmail.com>
2020-04-28 21:15:18 +0100

    Add email verification and anti spam feature tu the form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import Title from '../Title/Title'
+import ContactForm from './ContactForm'
 import './Contact.css'
 import {MdContentCopy} from 'react-icons/md'
 
@@ -37,12 +38,7 @@ export default function Contact() {
         </div>
       </div>
       <div className="right-side">
-        <form className="Contact-form animated forward fadeInRight" action="https://submit-form.com/iaCuGnpz2kraTRI8J6lWy" target="blank">
-          <input type="text" name="name" placeholder="Name" required/>
-          <input type="text" name="email" placeholder="Email" required/>
-          <textarea name="message" placeholder="Your message..." required></textarea>
-          <button type="submit">Send</button>
-        </form>
+        <ContactForm />
       </div>
     </>
   )

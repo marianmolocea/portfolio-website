c7e0d2e7bbd31037f654358e17411a80c6b21813
marianmolocea <marian.mcrt@gmail.com>
2020-04-28 20:36:48 +0100

    Request text in the contact form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -38,9 +38,9 @@ export default function Contact() {
       </div>
       <div className="right-side">
         <form className="Contact-form animated forward fadeInRight" action="https://submit-form.com/iaCuGnpz2kraTRI8J6lWy" target="blank">
-          <input type="text" name="name" placeholder="Name" />
-          <input type="text" name="email" placeholder="Email" />
-          <textarea name="message" placeholder="Your message..."></textarea>
+          <input type="text" name="name" placeholder="Name" required/>
+          <input type="text" name="email" placeholder="Email" required/>
+          <textarea name="message" placeholder="Your message..." required></textarea>
           <button type="submit">Send</button>
         </form>
       </div>
